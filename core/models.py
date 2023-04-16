ca64e3816481148db10bf82ee0f1b6221131aad3
PietroDona <dona.pietro@gmail.com>
2023-04-15 20:37:37 -0400

    model repr

--- core/models.py
+++ core/models.py
@@ -7,11 +7,17 @@ class Question(models.Model):
     title = models.CharField(max_length=100)
     text = models.TextField()
 
+    def __repr__(self) -> str:
+        return f"Question: {self.title}"
+
 
 class QuestionOption(models.Model):
     option = models.CharField(max_length=100)
     question = models.ForeignKey(
         Question, related_name='options', on_delete=models.CASCADE)
+
+    def __repr__(self) -> str:
+        return f"Option: {self.option} - of Q:{self.question.id} "
 
 
 class Vote(models.Model):
@@ -21,3 +27,6 @@ class Vote(models.Model):
         Question, related_name='votes', on_delete=models.CASCADE)
     content = models.ForeignKey(
         QuestionOption, on_delete=models.CASCADE, null=True)
+
+    def __repr__(self) -> str:
+        return f"Vote: {self.timestamp} - of Q:{self.question.id} "
